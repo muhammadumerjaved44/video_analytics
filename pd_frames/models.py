41af139adb1f3ea90a02117be633a7250dc22cd4
Muhammad Umer Javed <muhammadumerjaved44@gmail.com>
2021-11-10 12:38:04 +0500

    OMICRO-4 - Configre database with APIs using docker

--- pd_frames/models.py
+++ pd_frames/models.py
@@ -1,18 +1,18 @@
-from database import engine
+from database import engine, SessionLocal
 from sqlalchemy.sql import text
 
-
+ses = SessionLocal()
 async def insert_frames(data=None):
-    # data = { "frame_no": "The Hobbit", "video_name": "Tolkien"}
-
-    with engine.connect() as con:
+    # data = { "frame_no": "The Hobbit", "video_name": "Tolkien", 'file_path':'umer'}
+    sess =  SessionLocal()
+    with sess.connection() as con:
 
         if data is None:
             print('data is missing to inesrt')
             return
 
-        statement = text("""INSERT INTO table_2 (frame_no, video_name)\
-            VALUES (:frame_no, :video_name)""")
+        statement = text("""INSERT INTO table_2 (frame_no, video_name, file_path)\
+            VALUES (:frame_no, :video_name, :file_path)""")
 
         try:
             con.execute(statement, data)
