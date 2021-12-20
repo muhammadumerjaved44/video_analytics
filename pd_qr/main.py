3b0bb4378166f9257f2d825b3d73082effeb1c22
Muhammad Umer Javed <muhammadumerjaved44@gmail.com>
2021-12-20 11:24:14 +0500

    OMICRO-69 - Make API method for the QR code

---
+++ pd_qr/main.py
@@ -0,0 +1,74 @@
+import os
+import json
+from pathlib import Path
+import uvicorn
+from decouple import config
+from fastapi import BackgroundTasks, FastAPI, HTTPException, Request
+from fastapi.openapi.utils import get_openapi
+from qr_code import qr_to_text
+from models import get_unprocessed_qr_frame_url
+
+base_path = os.path.dirname(os.path.abspath(__file__))
+
+
+app = FastAPI()
+
+
+@app.post("/perform_qr_code_to_text", status_code=200, tags=["Qr Code"])
+async def perform_qr_code_to_text(background_tasks: BackgroundTasks, request: Request):
+    data = await request.json()
+    # image_path = data['image_url']
+    video_id = data['video_id']
+    frame_id = data['frame_id']
+    input_data = {'id':frame_id, 'video_id':video_id}
+    image_path = await get_unprocessed_qr_frame_url(input_data)
+    try:
+        if not image_path or len(image_path.strip()) == 0:
+            raise HTTPException(status_code=404, detail="image path is invalid or empty")
+        else:
+            background_tasks.add_task(qr_to_text, image_path,frame_id, video_id)
+        print({'video_id':video_id, 'frame_id':frame_id, 'image_path':image_path})
+        return {
+            'response': 'soon the predictions will be compeleted',
+            'file_name': image_path,
+        }
+    except:
+        print('image not processed for some reason')
+
+
+
+tags_metadata = [
+    {
+        "name": "decord",
+        "description": 'This End point used to extract the text information form QR Code',
+    }
+]
+
+description = """
+QR Code API 🚀
+"""
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Point Duty - QR Code API",
+        version="V-0.0.0",
+        description=description,
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    }
+    app.openapi_schema = openapi_schema
+    app.openapi_tags=tags_metadata
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
+
+
+if __name__ == "__main__":
+    # $ uvicorn main:app --reload
+    port = config('FASTAPI_LOCAL_PORT', cast=int)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload="True")
---
+++ pd_qr/qr_code.py
@@ -0,0 +1,68 @@
+from pyzbar import pyzbar
+import cv2
+import codecs
+import aiofiles
+import time
+import asyncio
+from urllib.parse import urlparse
+
+from models import (fetch_image_from_url, insert_object,
+                    update_qr_frame_flags)
+
+def timeit(func):
+    async def process(func, *args, **params):
+        if asyncio.iscoroutinefunction(func):
+            print('this function is a coroutine: {}'.format(func.__name__))
+            return await func(*args, **params)
+        else:
+            print('this is not a coroutine')
+            return func(*args, **params)
+
+    async def helper(*args, **params):
+        print('{}.time'.format(func.__name__))
+        start = time.time()
+        result = await process(func, *args, **params)
+
+        # Test normal function route...
+        # result = await process(lambda *a, **p: print(*a, **p), *args, **params)
+        final_time = time.time() - start
+        async with aiofiles.open('time_data.txt', mode='a') as f:
+            await f.write(f'\n{final_time},')
+        print('\n\n\nTotal execution time for this function = {} >>>'.format(func.__name__),final_time, '\n\n\n')
+        return result, final_time
+
+    return helper
+
+async def insert_qr_object(frame_no, video_name, data, frame_id, video_id):
+
+    response = {'frame_no':frame_no,
+                'frame_id':frame_id,
+                'video_id':video_id,
+                'video_name':video_name,
+                'object_':'qr_code',
+                'attribute_':'text',
+                'value_': data,
+                }
+
+    await insert_object(response)
+
+    update_data = {'frame_no':frame_no, 'video_name': video_name, 'is_qr_processed':1}
+    await update_qr_frame_flags(update_data)
+
+
+@timeit
+async def qr_to_text(main_file_url, frame_id, video_id):
+    frame_no = urlparse(main_file_url).path.split('_')[-1].split('.')[0]
+    video_name = urlparse(main_file_url).path.split('/')[-2]
+    image = await asyncio.gather(
+        asyncio.create_task(fetch_image_from_url(video_name, frame_no))
+        )
+    # image = cv2.imread(main_file_url)
+    barcodes = pyzbar.decode(image[0])
+    if barcodes:
+        final_string = codecs.decode(barcodes[0].data.decode(), 'unicode_escape').replace('\r', ' ').strip()
+    else:
+        final_string = ''
+
+    await insert_qr_object(frame_no, video_name, final_string, frame_id, video_id)
+    return final_string
